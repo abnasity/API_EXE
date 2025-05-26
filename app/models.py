f1e37c4e9e158abf8f21ff34b0df30cd3688803a
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-26 20:04:15 +0300

    add p to Primary_key

--- app/models.py
+++ app/models.py
@@ -1,12 +1,12 @@
 from .extensions import db
 
 class Course(db.Model):
-    id = db.Column(db.Integer, Primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
     students = db.relationship("student", back_populates="Course")
     
 class Student(db.Model):
-    id = db.Column(db.Integer, Primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
     course = db.Column(db.ForeignKey("course.id"))
     Course = db.relationship("course", back_populates="students")
