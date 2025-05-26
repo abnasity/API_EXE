a23aa01ba7bcf661fba61e0b9ec546a6a2f2da1c
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-26 19:54:17 +0300

    add rship

--- app/models.py
+++ app/models.py
@@ -3,10 +3,11 @@ from .extensions import db
 class Course(db.Model):
     id = db.Column(db.Integer, Primary_key=True)
     name = db.Column(db.String(50), unique=True)
-    
+    students = db.relationship("student", back_populates="Course")
     
 class Student(db.Model):
     id = db.Column(db.Integer, Primary_key=True)
     name = db.Column(db.String(50), unique=True)
     course = db.Column(db.ForeignKey("course.id"))
+    Course = db.relationship("course", back_populates="students")
     
