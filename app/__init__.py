7e1866f52caf492d57cbd8bfa31d8c3fc6c9501f
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-27 09:38:17 +0300

    initialize app

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from .extensions import api, db
+from.resources import ns
+
+def create_app():
+    app = Flask(__name__)
+
+    # Configuration
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    # Initialize extensions
+    api.init_app(app)
+    db.init_app(app)
+    
+    api.add_namespace(ns)
+
+    return app
